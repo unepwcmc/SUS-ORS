07d698dc6de67b82fb83861e22e947aae9c13bde
maxime <maxycle@hotmail.com>
2023-02-21 16:10:54 +0000

    feat:create account page with tabs

---
+++ nuxt-frontend/pages/account.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="flex justify-center">
+    <div v-if="isDetailsSelected" @click="toggleTab('contacts')">
+      <FTabsTabDetails class="w-[20rem]" />
+      <h1>Details</h1>
+    </div>
+    <div v-else @click="toggleTab('details')">
+      <FTabsTabContacts class="w-[20rem]" />
+      <h1>Contacts</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Account',
+
+  layout: 'default',
+
+  data () {
+    return {
+      tabSelected: 'details'
+    }
+  },
+
+  computed: {
+    isDetailsSelected () {
+      return this.tabSelected === 'details'
+    }
+  },
+
+  methods: {
+    toggleTab (tab) {
+      this.tabSelected = tab
+    }
+  }
+}
+</script>
