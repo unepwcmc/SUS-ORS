4e2df17b616b7194ed89cca46469c7b703b0abdf
maxime <maxycle@hotmail.com>
2023-02-21 17:51:08 +0000

    fix: add border to hero

--- nuxt-frontend/components/Frontend/Layout/Hero.vue
+++ nuxt-frontend/components/Frontend/Layout/Hero.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex items-center justify-between px-16">
+  <div class="flex items-center justify-between px-16 py-6 border-solid border-y-2 border-grey-accent">
     <div class="flex items-center">
       <img src="/MEA_Logo_Icon.jpg" class="h-[5rem]"></img>
       <div class="text-3xl ml-2">
