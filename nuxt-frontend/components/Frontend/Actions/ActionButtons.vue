57272d7be326da2e877db6db599bb7e5cb007fa8
maxime <maxycle@hotmail.com>
2023-02-24 11:50:38 +0000

    feat: add "more actions" buttons

---
+++ nuxt-frontend/components/Frontend/Actions/ActionButtons.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bg-grey-accent p-4">
+    <button class="flex items-center mb-4">
+      <FIconEdit class="w-8 mr-1" />
+      Edit
+    </button>
+    <button class="flex items-center">
+      <FIconDelete class="w-8 mr-1" />
+      Delete
+    </button>
+  </div>
+</template>
---
+++ nuxt-frontend/components/Frontend/Actions/MoreActionsButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="relative" @mouseover="onMouseOver" @mouseleave="onMouseLeave">
+      <FIconMoreIcons class="w-8" />
+      <FActionsActionButtons class="absolute top-0 left-0 z-10" :class="{ 'hidden': !displayActionButton }" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      hover: false
+    }
+  },
+
+  computed: {
+    displayActionButton () {
+      return this.hover
+    }
+  },
+
+  methods: {
+    onMouseOver () {
+      this.hover = true
+    },
+
+    onMouseLeave () {
+      this.hover = false
+    }
+  }
+}
+</script>
