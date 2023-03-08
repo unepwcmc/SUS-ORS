866522757e12936db133ce075e4f1784c58c138e
maxime <maxycle@hotmail.com>
2023-03-08 16:19:49 +0000

    fix: adjust margin/padding

--- nuxt-frontend/components/Frontend/Layout/Footer.vue
+++ nuxt-frontend/components/Frontend/Layout/Footer.vue
@@ -1,11 +1,11 @@
 <template>
-  <footer class="py-5 px-6 2xl:px-0 bg-grey-accent text-white">
+  <footer class="py-[3.1rem] px-[4.4rem] 2xl:px-0 bg-grey-accent text-white">
     <div class="w-auto mx-16 flex justify-between">
       <div class="text-black text-3xl pt-4">
         {{ title }}
       </div>
-      <div class="w-[10rem] py-4">
-        <FIconLogo class="w-full mb-3" />
+      <div class="w-[10rem]">
+        <FIconLogo class="w-full mb-6" />
         <div class="bg-blue-font flex justify-center py-2 px-2 rounded-sm w-full font-semibold mt-3" v-text="buttonSupportCenterText" />
       </div>
     </div>
