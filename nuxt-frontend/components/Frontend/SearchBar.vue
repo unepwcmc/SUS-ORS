fadd8b8f885c5c391cb5e1334cfd119b2d562569
maxime <maxycle@hotmail.com>
2023-03-10 13:04:52 +0000

    feat: implement search bar and header

---
+++ nuxt-frontend/components/Frontend/SearchBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="border border-grey-accent py-2.5 pl-5 pr-4 text-sm flex space-x-2.5">
+    <div>
+      &#128269;
+    </div>
+    <div>
+      {{ text }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: 'search something'
+    }
+  }
+}
+</script>
