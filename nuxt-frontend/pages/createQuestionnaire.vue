e0794e46841acf0d7d40d94ae613ad9ef0a5fba6
maxime <maxycle@hotmail.com>
2023-03-10 13:06:47 +0000

    feat: implement "create new questionnaire" page

--- nuxt-frontend/pages/createQuestionnaire.vue
+++ nuxt-frontend/pages/createQuestionnaire.vue
@@ -1,8 +1,14 @@
 <template>
-  <div>
-    <h1>
-      New qUestionnaire !!!!
-    </h1>
+  <div class="flex flex-col">
+    <FLayoutQuestionnaireHeader />
+    <div class="flex-grow flex">
+      <div class="bg-grey-bg w-1/4 pt-10 flex justify-end">
+        <FLayoutQuestionnaireCreateMenu />
+      </div>
+      <div class="bg-red-500 flex-grow">
+        right side !!
+      </div>
+    </div>
   </div>
 </template>
 
