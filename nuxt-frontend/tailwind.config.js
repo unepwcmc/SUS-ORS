ad5a6505a34019c24ba1d5c08b3b96efea6ecf00
maxime <maxycle@hotmail.com>
2023-03-14 13:47:30 +0000

    fix: comment out first line of tailwind.config

--- nuxt-frontend/tailwind.config.js
+++ nuxt-frontend/tailwind.config.js
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+// /** @type {import('tailwindcss').Config} */
 const screens = require('./tailwind-breakpoints.config.js')
 module.exports = {
   purge: {
